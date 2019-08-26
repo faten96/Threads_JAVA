c16cb1ac02063fed74b558a6a9d1350d494e746e
Faten <32636018+faten96@users.noreply.github.com>
2019-08-26 23:27:09 +0200

    ading the test of wait-notify to the main class

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
         int[] t={5,8,3,2,7,10,1};
-        Trieur trieur=new Trieur(t,0,t.length-1);
+        
+        // Use this code to test the first class
+        
+        
+       /* Trieur trieur=new Trieur(t,0,t.length-1);
         trieur.start();
         try {
             trieur.join();
@@ -13,7 +17,24 @@ public class Main {
         // diplay result
         for(int i=0;i<t.length;i++){
             System.out.print(t[i]+" | ");
+        }*/
+
+        // Use this code to test the second class
+        
+        
+       Trieur1 parent=new Trieur1();
+       Trieur1 trieur1=new Trieur1(parent,t,0,t.length-1);
+
+        try {
+            trieur1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
+        // diplay result
+        for(int i=0;i<t.length;i++){
+            System.out.print(t[i]+" | ");
+        }
     }
 }
+
